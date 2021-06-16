3aea6a45f1105d3824c20a0dce15ccefc3c1454a
xGogax <49436124+xGogax@users.noreply.github.com>
2021-06-16 11:27:56 +0200

    Kreiranje klase Jarvis
    Kreiranje klase Jarvis sa metodom za odredjivanje orijentacije.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,19 @@ public class Main {
         int y;
     }
 
+    public static class Jarvis {
+
+        private boolean orijentacija(Point p, Point q, Point r) {
+            int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+            if (val >= 0)
+                return false;
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
 	    
+        
     }
 }
